6034e023ffa211e884e61410cded3242863aa6d4
Diego Nogueira <diegof.fnogueira@gmail.com>
2019-08-22 17:18:50 -0300

    Curso de node

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var express = require('express');
+var express = require("express");
+var msg = require("./mod_teste")
+
 var app = express();
 
 app.set("view engine", "ejs");
@@ -17,4 +19,6 @@ app.get("/noticias", function (req, res) {
 
 app.listen(3000, function () {
     console.log("Servidor rodando com express.");
+    // console.log(msg);
+    console.log(msg());
 });
---
+++ mod_teste.js
@@ -0,0 +1,7 @@
+// var msg = "Este modulo contém apenas uma string";
+
+//module.exports = msg
+module.exports = function () {
+    var msg = "Este modulo contém apenas uma string";
+    return msg;
+}
