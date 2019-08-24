fcfce6d8ffa8f174d27f407b4423a5e8e1d16c3c
Diego Nogueira <diegof.fnogueira@gmail.com>
2019-08-23 22:16:00 -0300

    Aulas de nodejs

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -1,6 +1,28 @@
-module.exports = function(app){
+module.exports = function (app) {
+
     app.get("/noticias", function (req, res) {
-        res.render("noticias/noticias")
+
+        var mysql = require("mysql");
+
+        var connection = mysql.createConnection({
+            host: "localhost",
+            user: "diego",
+            password: "1234",
+            port: "3306",
+            database: "portal_noticias"
+        });
+
+        connection.connect();
+
+        connection.query('select * from noticias', function (error, result) {
+            console.log(error);
+            console.log(result);
+            res.send(result);
+        });
+
+        // res.render("noticias/noticias")
+
     });
-}
+
+};
 
