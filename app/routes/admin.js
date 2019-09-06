e0bb9c50af1d47d254d62c1b25693818e73b84eb
Diego Nogueira <diegof.fnogueira@gmail.com>
2019-09-05 22:39:56 -0300

    Noticias com Nodejs

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,19 +1,47 @@
+const {
+    check,
+    validationResult
+} = require('express-validator');
+
 module.exports = function (application) {
+
     application.get("/formulario_inclusao_noticia", function (req, res) {
-        res.render("admin/form_add_noticia");
-    });
-
-    application.post("/noticias/salvar", function (req, res) {
-        var noticia = req.body;
-        // res.send(noticias);
-
-        var conn = application.config.dbConnection();
-        var noticiasDAO = new application.app.models.NoticiasDAO(conn);
-
-        noticiasDAO.salvarNoticia(noticia, function (error, result) {
-            res.redirect('/noticias');
+        res.render("admin/form_add_noticia", {
+            validacao: {}
         });
-
-
     });
+
+    application.post("/noticias/salvar",
+        [
+            check('titulo').not().isEmpty().withMessage('O titulo é obrigatório.').isLength({
+                max: 10
+            }).withMessage('O titulo está muito grande.'),
+            check('resumo').not().isEmpty().withMessage('O resumo é obrigatório.').isLength({
+                min: 10,
+                max: 100
+            }).withMessage('O resumo deve ter entre 10 e 100 caracteres.'),
+            check('autor').not().isEmpty().withMessage('O autor é obrigatório.'),
+            check('data_noticia').not().isEmpty().withMessage('A data é obrigatória.'),
+            check('noticia', 'A notícia é obrigatória.').not().isEmpty()
+        ],
+        function (req, res) {
+            var noticia = req.body;
+            // res.send(noticias);
+
+            var errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                return res.status(422).render('admin/form_add_noticia', {
+                    validacao: errors.array()
+                });
+            }
+
+            var conn = application.config.dbConnection();
+            var noticiasDAO = new application.app.models.NoticiasDAO(conn);
+
+            noticiasDAO.salvarNoticia(noticia, function (error, result) {
+                res.redirect('/noticias');
+            });
+
+        });
 };
--- noticias.js
+++ noticias.js
@@ -5,13 +5,13 @@ var server = http.createServer(function (req, res) {
 
     var categoria = req.url;
     if (categoria === "/tecnologia") {
-        res.end("<html><body>Portal de Tecnologia</body></html>")
+        res.end("<html><body>Portal de Tecnologia</body></html>");
     } else if (categoria === "/moda") {
-        res.end("<html><body>Portal de Moda</body></html>")
+        res.end("<html><body>Portal de Moda</body></html>");
     } else if (categoria === "/beleza") {
-        res.end("<html><body>Portal de Beleza</body></html>")
+        res.end("<html><body>Portal de Beleza</body></html>");
     } else {
-        res.end("<html><body>Portal de Notícias</body></html>")
+        res.end("<html><body>Portal de Notícias</body></html>");
     }
 });
 
